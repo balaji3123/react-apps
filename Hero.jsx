4b4ef1d466ac7c6cb8615cae4fd769024e7f8649
balaji3123 <165598087+balaji3123@users.noreply.github.com>
2024-04-01 01:58:38 +0530

    Add files via upload

---
+++ Hero.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <React.Fragment>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+        <div className='centered h-screen -mt-20'>
+            <div className = 'text-white'>
+                <div>
+                    <p>
+                        Why Do We Fall Bruce?
+                    </p>
+                
+
+                </div>
+                <div className='butclass'>
+                    <button>Rise</button>
+                    <button>Pick ourselves up</button>
+                </div>
+                
+            
+
+            </div>
+        </div>
+    </React.Fragment>
+    
+  )
+}
+
+export default Hero
---
+++ Mainpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+
+const Mainpage = () => {
+  return (
+    <React.Fragment>
+        <section>
+            <div className = 'wrapper'>
+                <Hero/>
+            </div>
+        </section>
+    </React.Fragment>
+  )
+}
+
+export default Mainpage
